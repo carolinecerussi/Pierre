3853de21c39372d35a518542e8895807dad17d98
caroline cerussi <caroceru@gmail.com>
2022-07-22 10:34:41 -0700

    add method of getting new name as string and pass

--- Pierre.Tests/ModelTests/VendorTests.cs
+++ Pierre.Tests/ModelTests/VendorTests.cs
@@ -6,21 +6,22 @@ using System;
 namespace Pierre.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class VendorTests
   {
+
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
-    // [Testmethod]
-    // public void GetVendorName_ReturnsVendorName_String()
-    // {
-    //   string vendorName = "test name";
-    //   Vendor newVendor = new Vendor(name);
-    //   string result = newVendor.Name;
-    //   Assert.AreEqual(name, result);
-    // }
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "test name";
+      Vendor newVendor = new Vendor(vendorName);
+      string result = newVendor.VendorName;
+      Assert.AreEqual(vendorName, result);
+    }
   }
 }
